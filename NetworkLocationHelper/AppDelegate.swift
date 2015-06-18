972351f7f83bbb1b9543f15f2279a5769c4029ff
Cameron Carmichael Alonso <Cameron@Camerons-MacBook-Pro.local>
2015-06-18 20:04:28 +0200

    Initial Commit

---
+++ NetworkLocationHelper/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NetworkLocationHelper
+//
+//  Created by Cameron Carmichael Alonso on 18/06/15.
+//  Copyright (c) 2015 Cameron Carmichael Alonso. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
